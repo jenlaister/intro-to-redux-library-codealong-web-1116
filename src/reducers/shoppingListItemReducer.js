403ed1abe12ec43c4f9ef98143fb664a9468900f
Jenny Lai <jenny.lai@flatironschool.com>
2017-02-07 18:40:16 -0500

    Read

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,6 +1,11 @@
 export default function shoppingListItemReducer(state = {items: []}, action) {
-  console.log(action)
   switch(action.type) {
+    // case 'INCREASE_COUNT'
+    // let items = items.concat(state.items.length + 1)
+    // return Object.assign({}, {items: items})
+    // default:
+    // return state
+    // console.log(action)
     case 'GET_COUNT_OF_ITEMS':
     let items;
     items = state.items.concat(state.items.length + 1)
